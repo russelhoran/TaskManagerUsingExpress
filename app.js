0e5ed88e58aa29f0c4411dfbe6602d728388511d
russelhoran.russelfernandes <russelhoran.russelfernandes35@gmail.com>
2023-07-05 20:22:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const tasks= require('./routes/tasks')
+const connectDB= require('./db/connect')
+require('dotenv').config()
+const notFound= require('./middleware/not-found')
+const errorhandler = require('./middleware/errorhandler')
+
+
+//Middleware
+app.use(express.json())
+app.use(express.static('./public'))
+
+//routes 
+app.use('/api/v1/tasks',tasks)
+app.use(notFound)
+app.use(errorhandler)
+
+const port = process.env.PORT || 3000
+const start= async ()=>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
+
+
+
+
